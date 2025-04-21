f103bb869b174f56161f93b823fd1549795e52fa
Redmi1312 <redmi1312mi@gmail.com>
2025-04-21 11:40:53 +0300

    Add files via upload

---
+++ CarDeformation/Scripts/body_deform.gd
@@ -0,0 +1,34 @@
+extends MeshInstance3D
+
+var mesh_data_tool = MeshDataTool.new()
+var deform_timer = 0.0
+
+func _ready():
+	if mesh and mesh.get_surface_count() > 0:  # Проверяем, есть ли поверхности
+		mesh_data_tool.create_from_surface(mesh, 0)
+
+func apply_deformation(position: Vector3, force: float, radius: float):
+	if mesh_data_tool.get_vertex_count() == 0:
+		return
+	
+	for i in range(mesh_data_tool.get_vertex_count()):
+		var vertex_pos = mesh_data_tool.get_vertex(i)
+		var distance = vertex_pos.distance_to(position)
+		
+		if distance < radius:
+			var deformation = (1.0 - (distance / radius)) * force
+			vertex_pos += Vector3(0, -deformation, 0)  # Деформация вниз
+			mesh_data_tool.set_vertex(i, vertex_pos)
+	
+	deform_timer = 0.1  # Обновлять меш раз в 0.1 сек
+
+func _process(delta):
+	if deform_timer > 0:
+		deform_timer -= delta
+		if deform_timer <= 0:
+			update_mesh()
+
+func update_mesh():
+	var new_mesh = ArrayMesh.new()
+	mesh_data_tool.commit_to_surface(new_mesh)
+	mesh = new_mesh
